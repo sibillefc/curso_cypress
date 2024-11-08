3b04e5019d19faefc9000b062fb92ee73aefd7d0
fabio <sibillefc@gmail.com>
2024-11-08 16:17:04 -0300

    ocultando bear

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,11 +1,9 @@
 describe('API Adopet', () => {
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIwYjE3NjRkNS0yNDJmLTRkN2EtODk0MC05NzQ4MGQ2NTMxM2EiLCJhZG9wdGVyTmFtZSI6ImZhYmlvIiwiaWF0IjoxNzMxMDEyMzM4LCJleHAiOjE3MzEyNzE1Mzh9.xc6pxUe-TTaJZzH5pQyUOwZurfgJXQBjLXZUlu0rboo`
-    
     it ('Mensagem da API',() =>{
         cy.request({
             method: 'GET' ,
             url: 'https://adopet-api-i8qu.onrender.com/mensagem/0b1764d5-242f-4d7a-8940-97480d65313a',
-            headers: { authorization }            
+            headers: Cypress.env()            
         }).then((res) => {
             expect (res.status).to.be.equal(200)
             expect (res.body).is.not.empty
